013a99f4c440bfd7e2e5663154bacd7106b04de6
clooux <55985737+clooux@users.noreply.github.com>
2023-03-24 14:53:23 +0100

    routes for admin login and button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Rooms from "./components/Rooms";
 import Reservation from "./components/Reservation";
 import ImportData from "./components/ImportData";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="sale" element={<Rooms />} />
           <Route path="rezerwacja" element={<Reservation />} />
           <Route path="import" element={<ImportData />} />
+          <Route path="login" element={<Login />} />
           <Route path="*" element={<div>Nie znaleziono strony </div>} />
         </Route>
       </Routes>
