e319120964fee8c10880c8ac73b3646303620597
miccu64 <98konrad98@gmail.com>
2023-03-28 09:40:03 +0200

    Rooms filtering

--- backend/src/main/java/com/wpz/rbs/model/usos/RoomsUsos.java
+++ backend/src/main/java/com/wpz/rbs/model/usos/RoomsUsos.java
@@ -1,9 +1,7 @@
 package com.wpz.rbs.model.usos;
 
-import com.wpz.rbs.model.Room;
-
 import java.util.List;
 
 public class RoomsUsos {
-    public List<Room> rooms;
+    public List<RoomUsos> rooms;
 }
--- backend/src/main/java/com/wpz/rbs/service/RoomImportService.java
+++ backend/src/main/java/com/wpz/rbs/service/RoomImportService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class RoomImportService {
@@ -21,8 +22,9 @@ public class RoomImportService {
         GenericUrl genericUrl = new GenericUrl("https://apps.usos.uj.edu.pl/services/geo/building2");
         genericUrl.set("building_id", "Loj11");
         genericUrl.set("langpref", "pl");
-        genericUrl.set("fields", "rooms");
-        RoomsUsos roomsUsos = new ObjectMapper().readValue(usosAuthService.usosApiRequest(genericUrl).parseAsString(), RoomsUsos.class);
-        return roomsUsos.rooms.stream().map(room -> new Room(room.id, room.number)).toList();
+        genericUrl.set("fields", "rooms[id|number|type]");
+        String json = usosAuthService.usosApiRequest(genericUrl).parseAsString();
+        RoomsUsos roomsUsos = new ObjectMapper().readValue(json, RoomsUsos.class);
+        return roomsUsos.rooms.stream().filter(room -> Objects.equals(room.type, "didactics_room")).map(room -> new Room(room.id, room.number)).toList();
     }
 }
