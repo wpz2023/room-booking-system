cd04bbfcc72dc9bceec4b171cac449b7ce909fa1
Denwiliusz27 <danwielgosz@gmail.com>
2023-03-29 22:19:46 +0200

    login view v1.0

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -2,21 +2,28 @@ import React from 'react'
 
 function Login() {
   return (
-    <div className="flex justify-center">
-        <div className='p-6 w-2/5' >
-          <form className="flex flex-col w-full ">
-            <label className="flex flex-col ph-20">
-              <span className="flex py-2 after:content-['*'] after:text-red-600">Email</span>
+    <div className="flex flex-col items-center pt-20">
+        <div className="w-2/5 pb-14 text-center text-2xl font-medium">
+            <p>Logowanie</p>
+        </div>
+        <div className='w-96'>
+          <form className=" ">
+            <label className="flex flex-col py-2">
+              <span className="flex py-2 after:content-['*'] after:text-red-600 font-bold">Email</span>
               <input type="email" name="email" placeholder={"adres@email"}
-                        className="peer py-2 px-5 border focus:border-b-blue-500 focus:ring-blue-500 rounded-md"></input>
+                        className="peer py-2 px-5 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"></input>
               <p className="py-2 invisible peer-invalid:visible text-red-600">Nieprawidłowy adres email</p>
             </label>
-            <label className="flex flex-col ph-20">
-                <span className="flex py-2 after:content-['*'] after:text-red-600">Hasło</span>
+            <label className="flex flex-col py-2">
+                <span className="flex py-2 after:content-['*'] after:text-red-600 font-bold">Hasło</span>
                 <input type="password" name="password"
-                       className="peer py-2 px-5 border focus:border-b-blue-500 focus:ring-blue-500 rounded-md"></input>
+                       className="peer py-2 px-5 border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"></input>
             </label>
-            <input type="submit" value="Zaloguj" className="w-15 h-15 justify-end"></input>
+            <div className="flex justify-center">
+                <input type="submit" value="Zaloguj"
+                       className="my-12 px-12 py-2  transition hover:scale-110 delay-150 rounded-lg bg-sky-500 hover:bg-sky-700 hover:shadow-sky-700 text-white shadow-lg shadow-sky-500"></input>
+            </div>
+
           </form>
         </div>
     </div>
