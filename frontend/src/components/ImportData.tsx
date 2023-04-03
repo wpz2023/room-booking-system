269c7372f3e7ec235d67a1c0f6d864ff92bfd9b7
Denwiliusz27 <danwielgosz@gmail.com>
2023-04-03 22:20:13 +0200

    styled list view

--- frontend/src/components/ImportData.tsx
+++ frontend/src/components/ImportData.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { RoomData } from "../models/Room";
 import { useQuery } from "@tanstack/react-query";
 import Api from "../Api";
-import {Link} from "react-router-dom";
 
 
 function ImportData() {
@@ -17,7 +16,7 @@ function ImportData() {
 
   };
 
-  const { isLoading, isFetching, data, refetch} = useQuery<RoomData[]>( ["data"], emulateFetch, {
+  const { isFetching, data, refetch} = useQuery<RoomData[]>( ["data"], emulateFetch, {
     refetchOnWindowFocus: false,
     enabled: false
   });
@@ -31,20 +30,24 @@ function ImportData() {
         bg-sky-500 hover:bg-sky-700 hover:shadow-sky-700 text-white shadow-lg shadow-sky-500" onClick={handleClick}>
           Zaimportuj dane
         </button>
-        {!isLoading && isFetching ? <p>Ładowanie danych</p> : (
-        <ul role="list" className="w-full p-6 divide-y divide-slate-200">
-          {data?.map((room) => (
-              <li className="first:pt-0 last:pb-0" key={room.id}>
-                <div className="flex justify-evenly">
-                  <Link to={"/sale/" + room.id.toString()}>{room.number}</Link>
-                  <div className="">{room.type == "didactics_room" ? "Sala dydatktyczna" : room.type}</div>
-                  <div className="">{room.capacity}</div>
-                  <button className="mb-14 px-12 py-2  transition hover:scale-110 delay-150 rounded-lg
+        {isFetching ? <p>Ładowanie danych</p> : (
+            <div>
+                {data && <div className="w-[450px] px-6">
+                    <p className="font-medium">Numer sali</p>
+                    <hr className="h-px my-3 bg-gray-200 border-0 h-0.5 dark:bg-gray-700"/>
+                    </div>}
+                <ul role="list" className="w-[450px] p-6 divide-y divide-slate-200">
+                    {data?.map((room) => (
+                        <li className="first:pt-0 last:pb-0 py-8" key={room.id}>
+                            <div className="flex text-center items-center">
+                                <p className="basis-3/5 font-medium">{room.number}</p>
+                                <button className="basis-2/5  px-12 py-2  transition hover:scale-110 delay-150 rounded-lg
         bg-sky-500 hover:bg-sky-700 hover:shadow-sky-700 text-white shadow-lg shadow-sky-500">Importuj</button>
-                </div>
-              </li>
-          ))}
-        </ul>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         )}
       </div>
   );
